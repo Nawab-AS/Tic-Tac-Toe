e8f004b8daa2262b8c6cdf7b8f62f4d78e3d0595
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2026-04-09 18:44:54 -0400

    added simple UI proxy

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const table = document.getElementById('board').children;
-var board = new Array(3).fill(new Array(3))
+var rawBoard = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+const boardHandler = {
+    get(target, prop) {
+        const [x, y] = prop.split(",");
+        return target[y][x];
+    },
+    set(target, prop, value) {
+        const [x, y] = prop.split(",");
+        target[y][x] = value;
+        table[y].children[x].innerText = value;
+        return true;
+    }
+}
+const board = new Proxy(rawBoard, boardHandler);
 
-
+currentPlayer = "X";
 
 function buttonPressed(event) {
     const [buttonX, buttonY] = event.srcElement.pos
     console.log(`button pressed: row #${buttonY+1}, column#${buttonX+1}`)
+    board[`${buttonX},${buttonY}`] = currentPlayer;
+    
+    // Switch players
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
 
 // initialize board buttons
 for (let buttonY = 0; buttonY < table.length; buttonY++) {
-    // console.log(table[buttonX])
     for (let buttonX = 0; buttonX < table[buttonY].children.length; buttonX++) {
         let button = table[buttonY].children[buttonX];
-        // console.log(button)
-        button.pos = [buttonX, buttonY]
+        button.pos = [buttonX, buttonY];
         button.onclick = buttonPressed;
     }
 }
